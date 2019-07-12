dacc4b25272f8f2bd24f8bb006190392e6252707
blackbad1 <fawcetteugene@gmail.com>
2019-07-12 16:27:58 +0300

    Add a function to access the value of selected radio button.

---
+++ js/main.js
@@ -0,0 +1,9 @@
+//Function to get value of selected button
+var genders= document.getElementById('genders').value;
+if (document.getElementById('g1').checked){
+  gender_value=document.getElementById('g1').value;
+} else if (document.getElementById('g2').checked) {
+  gender_value=document.getElementById('g2').value;
+} else if (document.getElementById('g3').checked) {
+  gender_value=document.getElementById('g3').value;
+}
