ef006e41bb3c3ede4f94326c7e4326e65fa4fc8f
blackbad1 <fawcetteugene@gmail.com>
2019-07-12 16:29:22 +0300

    Create arrays for the weekdays, male names, and female names.

--- js/main.js
+++ js/main.js
@@ -7,3 +7,7 @@ if (document.getElementById('g1').checked){
 } else if (document.getElementById('g3').checked) {
   gender_value=document.getElementById('g3').value;
 }
+//Create arrays
+ var weekDays=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+ var maleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+ var femaleNames=["Akosua","Adwao","Abenaa","Akua","Yaa","Afua","Ama"];
