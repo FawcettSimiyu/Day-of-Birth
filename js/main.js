8e783101dee58bda93aa255de06feab366b3e89d
blackbad1 <fawcetteugene@gmail.com>
2019-07-12 17:01:43 +0300

    Add form where user can enter personal birthday information.

--- js/main.js
+++ js/main.js
@@ -8,6 +8,6 @@ if (document.getElementById('g1').checked){
   gender_value=document.getElementById('g3').value;
 }
 //Create arrays
- var weekDays=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
- var maleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
- var femaleNames=["Akosua","Adwao","Abenaa","Akua","Yaa","Afua","Ama"];
+var weekDays=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+var maleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+var femaleNames=["Akosua","Adwao","Abenaa","Akua","Yaa","Afua","Ama"];
